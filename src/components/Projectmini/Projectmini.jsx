a5064febd2f0d46d87d4b4697dd9a29a8b38b29a
nashnc <nash.nc@gmail.com>
2025-06-01 13:33:46 +0530

    improved folder organizing and ui improvements

--- src/components/Projectmini/Projectmini.jsx
+++ src/components/Projectmini/Projectmini.jsx
@@ -5,6 +5,7 @@ import warehouse from "../Images/projectPics/warehouse.png";
 import port from "../Images/projectPics/port.png";
 import ott from "../Images/projectPics/ott.png";
 import soon from "../../assets/soon.webp";
+import "../css/ProjectCard.css";
 
 const useHoverCapability = () => {
   const [hasHoverCapability, setHasHoverCapability] = useState(true);
